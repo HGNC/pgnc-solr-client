506ea2c55678b3ace557940661f1ded2d899836e
Kris Gray <kag56@cam.ac.uk>
2025-02-19 23:07:25 +0000

    Refactor app configuration to use static values for environment and API version

--- src/config/environment.validation.ts
+++ src/config/environment.validation.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi';
 
 export default Joi.object({
   NODE_ENV: Joi.string().valid('dev', 'prod', 'test', 'stage').default('dev'),
-  API_VERSION: Joi.string().required(),
+  //   API_VERSION: Joi.string().required(),
   SOLR_HOST: Joi.string().required(),
   SOLR_PORT: Joi.number().required(),
   SOLR_CORE: Joi.string().required(),
