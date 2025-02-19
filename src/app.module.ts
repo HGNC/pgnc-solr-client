07aa3f961653cb79bfb0e292a89d488c83a03a5e
Kris Gray <kag56@cam.ac.uk>
2025-02-19 17:56:06 +0000

    Comment out environment variable handling in app module for debugging purposes

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,14 +7,14 @@ import appConfig from './config/app.config';
 import environmentValidation from './config/environment.validation';
 import solrConfig from './config/solr.config';
 
-const env = process.env.NODE_ENV;
+// const env = process.env.NODE_ENV;
 
 @Module({
   imports: [
     BrowseModule,
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: !env ? '.env' : `.env.${env}`,
+      //   envFilePath: !env ? '.env' : `.env.${env}`,
       load: [appConfig, solrConfig],
       validationSchema: environmentValidation,
     }),
